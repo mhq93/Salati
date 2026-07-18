43c651b608815a41f0604c7f4886cbb6b9af8ef7
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-07-18 12:24:07 +0300

    Set up Ktor client

---
+++ app/src/main/java/com/mhq/salati/data/remote/KtorClient.kt
@@ -0,0 +1,25 @@
+package com.mhq.salati.data.remote
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logging
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object KtorClient {
+    val instance: HttpClient by lazy {
+        HttpClient(CIO) {
+            install(ContentNegotiation) {
+                json(Json {
+                    ignoreUnknownKeys = true
+                    isLenient = true
+                })
+            }
+            install(Logging) {
+                level = LogLevel.INFO
+            }
+        }
+    }
+}
