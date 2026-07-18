3902cf453e960c157a8532ecb8aa1102d9f96b68
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-07-18 14:51:34 +0300

    Added Home Container/Content Mock UI Design

--- app/src/main/java/com/mhq/salati/presentation/home/HomeContainer.kt
+++ app/src/main/java/com/mhq/salati/presentation/home/HomeContainer.kt
@@ -1,2 +1,23 @@
 package com.mhq.salati.presentation.home
 
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+@Composable
+fun HomeContainer(
+    viewModel: HomeViewModel = viewModel()
+) {
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(Unit) {
+        viewModel.onIntent(HomeContract.Intent.LoadPrayerTimes)
+    }
+
+    HomeContent(
+        state = state,
+        onIntent = viewModel::onIntent
+    )
+}
--- app/src/main/java/com/mhq/salati/presentation/home/HomeContent.kt
+++ app/src/main/java/com/mhq/salati/presentation/home/HomeContent.kt
@@ -1,2 +1,51 @@
 package com.mhq.salati.presentation.home
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeContent(
+    state: HomeContract.State,
+    onIntent: (HomeContract.Intent) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        when {
+            state.isLoading -> {
+                CircularProgressIndicator()
+            }
+
+            state.errorMessage != null -> {
+                Text(text = "Error: ${state.errorMessage}")
+                Button(onClick = { onIntent(HomeContract.Intent.Retry) }) {
+                    Text("Retry")
+                }
+            }
+
+            state.timings != null && state.date != null -> {
+                Text(text = state.date.readable)
+                Text(text = "Fajr: ${state.timings.fajr}")
+                Text(text = "Sunrise: ${state.timings.sunrise}")
+                Text(text = "Dhuhr: ${state.timings.dhuhr}")
+                Text(text = "Asr: ${state.timings.asr}")
+                Text(text = "Maghrib: ${state.timings.maghrib}")
+                Text(text = "Isha: ${state.timings.isha}")
+            }
+        }
+    }
+}
