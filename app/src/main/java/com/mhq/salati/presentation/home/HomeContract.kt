9314c824d797824f279ba8ecef1910409f9dab1b
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-07-18 14:37:17 +0300

    Set up the contract for Home Screen

--- app/src/main/java/com/mhq/salati/presentation/home/HomeContract.kt
+++ app/src/main/java/com/mhq/salati/presentation/home/HomeContract.kt
@@ -1,2 +1,23 @@
 package com.mhq.salati.presentation.home
 
+import com.mhq.salati.domain.model.PrayerDate
+import com.mhq.salati.domain.model.PrayerTimings
+
+class HomeContract {
+
+    data class State(
+        val isLoading: Boolean = false,
+        val timings: PrayerTimings? = null,
+        val date: PrayerDate? = null,
+        val errorMessage: String? = null
+    )
+
+    sealed interface Intent {
+        data object LoadPrayerTimes : Intent
+        data object Retry : Intent
+    }
+
+    sealed interface Effect {
+        data class ShowError(val message: String) : Effect
+    }
+}
