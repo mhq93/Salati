7bc0d858b51a16ab1f1dd2311d4139b042e8c138
Mohamed Hamed Qaddal <mohamed.h.qaddal@gmail.com>
2026-07-18 13:18:50 +0300

    Resolved gradle issues by changing compileSdk from 36 to 37

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.mhq.salati"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
